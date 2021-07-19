91e5e3125324f569f0c4186fce66e6c27dbed47d
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-07-19 13:04:01 -0300

    feat: implement clear params feature

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function compareParams(param1, param2) {
 function validateParams(param1, param2) {
   return param1 && param2
 }
+
+function clearParams(param1, param2) {
+  param1 = null
+  param2 = null
+  return
+}
